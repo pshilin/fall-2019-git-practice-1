6e7248ccce282f91331c78ec0919404f147ce15a
CybertekSchool <33201481+CybertekSchool@users.noreply.github.com>
2020-03-18 20:05:07 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
         System.out.println("someone");
+        System.out.println("change one");
     }
 }
